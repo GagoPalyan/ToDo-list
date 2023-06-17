c46cbca08d1210e540015cf556f23cde4dff6664
GagoPalyan <gagik.palyan2002@mail.ru>
2023-06-17 15:37:44 +0400

    this is the fix bugs

--- src/TodoList.js
+++ src/TodoList.js
@@ -18,23 +18,3 @@ function TodoList({ todos, onDelete, onChange }) {
 }
 
 export default TodoList;
-
-// import TodoItem from "./TodoItem";
-
-// function TodoList({ todos, onChange, onDelete }) {
-//   return (
-//     <div>
-//       {todos.map((todo) => {
-//         return (
-//           <TodoItem
-//             key={todo.id}
-//             todo={todo}
-//             onChange={onChange}
-//             onDelete={onDelete}
-//           />
-//         );
-//       })}
-//     </div>
-//   );
-// }
-// export default TodoList;
